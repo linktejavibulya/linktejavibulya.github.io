35bd44d52b5fe81220cbe0a107c59c2c52247412
Link Tejavibulya <54546507+linktejavibulya@users.noreply.github.com>
2023-08-25 09:44:30 -0400

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,27 +1,20 @@
 // JavaScript Document
 
-let main_banner = document.getElementById("main-banner");
 let navbar = document.getElementById("main-nav");
-let navPos = navbar.getBoundingClientRect().top;
+let viewportHeight = window.innerHeight;
+let navHeight = document.getElementById("main-nav").offsetHeight;
+
 let navbarLinks = document.querySelectorAll("nav a");
 
 window.addEventListener("scroll", e => {
-  
-  let scrollPos = window.scrollY;
-  if (scrollPos > navPos) {
-    navbar.classList.add('sticky');
-    header.classList.add('navbarOffsetMargin');
-  } else {
-    navbar.classList.remove('sticky');
-    header.classList.remove('navbarOffsetMargin');
-  }
-  
-    navbarLinks.forEach(link => {
-      let section = document.querySelector(link.hash);
-      if (scrollPos + 150 > section.offsetTop && scrollPos + 150 < section.offsetTop + section.offsetHeight ) {
-        link.classList.add("active");
-      } else {
-        link.classList.remove("active");
-      }
-    });
+  scrollpos = window.scrollY;
+  navbarLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+    if (section.offsetTop <= scrollpos + 150 &&
+        section.offsetTop + section.offsetHeight > scrollpos + 150) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
 });
